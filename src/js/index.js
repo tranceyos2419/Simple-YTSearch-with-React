0357cc2168edff9c925c589807009fd3311e85db
root <root@DESKTOP-EIVIDTR.localdomain>
2018-07-02 11:43:37 +0900

    React and Redux Template 01

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+import '../../views/index.html';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+// import '../index.html'
+
+import App from './components/app';
+import reducers from './reducers/index';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
