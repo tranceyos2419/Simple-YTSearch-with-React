63d3765c3f53d0a8444259c471faf0a9a5500eba
tranceyos2419 <tranceyos2419@gmail.com>
2018-07-02 21:30:25 +0900

    add prettier

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,18 @@
-import '../../views/index.html';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import "../../views/index.html";
+import React from "react";
+import ReactDOM from "react-dom";
 
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 // import '../index.html'
 
-import App from './components/app';
-import reducers from './reducers/index';
+import App from "./components/app";
+import reducers from "./reducers/index";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
-
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
-  </Provider>
-  , document.querySelector('.container'));
+  </Provider>,
+  document.querySelector(".container")
+);
